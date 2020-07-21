7bfe88e0b2a40111a402ece2488645c687d6dbc1
Amit Upadhyay <amitu2016@gmail.com>
2020-07-21 23:46:14 +0530

    Mouse click Program

--- mouse_click.py
+++ mouse_click.py
@@ -3,5 +3,6 @@ import pyautogui
 #.moveTo(31,637,duration=2)
 #Automatic commit
 pyautogui.click(1495,44,duration=2)
-pyautogui.click(72,547,duration=2)
-pyautogui.click(31,637,duration=2)
+pyautogui.click(110,548,duration=2)
+pyautogui.click(52,572,duration=2)
+pyautogui.click(34,635,duration=2)
