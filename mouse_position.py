c8b01ce135db573bcd31f355bbcb30cb3e2f9681
Amit Upadhyay <amitu2016@gmail.com>
2020-07-21 23:11:27 +0530

    Mouse position Program

---
+++ mouse_position.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+pyautogui.moveTo(1000,800,duration=1)
+pyautogui.moveTo(500,500,duration=1)
+pyautogui.moveTo(700,700,duration=1)
+pyautogui.moveTo(800,800,duration=1)
+pyautogui.moveTo(1000,800,duration=1)
